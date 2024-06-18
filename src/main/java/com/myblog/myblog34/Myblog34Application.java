2e6c55bc9526616befdf82aec6b86238b5732b23
Sharath <sharath91130@gmail.com>
2024-06-18 12:56:41 +0530

    127:create save post feature

---
+++ src/main/java/com/myblog/myblog34/Myblog34Application.java
@@ -0,0 +1,13 @@
+package com.myblog.myblog34;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog34Application.class, args);
+	}
+
+}
